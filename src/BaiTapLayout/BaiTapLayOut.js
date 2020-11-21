b5ed09cfe1ace6d5b063a04ee1a22e56fe6a590e
tranbason241@gmail.com <tranbason241@gmail.com>
2020-11-21 17:08:40 +0700

    buoi 1 dataBiding

---
+++ src/BaiTapLayout/BaiTapLayOut.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Footer from './FooterComponent'
+import Header from './HeaderComponent'
+import Product from './ProductComponent'
+import Slider from './SliderComponent'
+
+export default class BaiTapLayout extends Component {
+    render() {
+        return (
+            <div>
+                {/* <Header />
+                <Slider />
+                <Product />
+                <Footer /> */}
+
+            </div>
+        )
+    }
+}
---
+++ src/DemoFuntionComponent.js
@@ -0,0 +1,21 @@
+
+//Cú pháp rfc
+import React from 'react'
+
+export default function DemoFuntionComponent() {
+    return ( //nội dung thẻ chứa trong lện returrn lưu ý: Nội dung phải đc bao bọc trong MỘT thẻ duy nhất
+
+        <div className="container bg-dark text-white">
+            <div className="display-4 p-5">
+                Component
+     </div>
+            <div className="p-2 pl-5">
+                Nội dung component
+    </div>
+
+        </div>
+
+    )
+}
+
+
